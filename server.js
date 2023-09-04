4d47e520bbf99f94a6ff410ec8bde97abb45b84f
jeremysr <jeremysr@protonmail.com>
2023-09-04 13:30:03 -0600

    added route for /notes but currently not working

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/notes", (req, res) => {
+    console.log(`GET request received at /notes`);
+    res.sendFile("notes.html");
+})
+
 
 app.listen(PORT, () =>
   console.log(`Serving static asset routes at http://localhost:${PORT}!`)
