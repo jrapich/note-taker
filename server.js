49927ee2e5445aa43c8245949ba0d160bf3c72cc
jeremysr <jeremysr@protonmail.com>
2023-09-04 13:00:34 -0600

    added app listener for express

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const app = express();
 const PORT = process.env.port || 3001;
 
 app.use(express.static('public'));
+
+
+app.listen(PORT, () =>
+  console.log(`Serving static asset routes at http://localhost:${PORT}!`)
+);
