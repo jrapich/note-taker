1e7f77d9109cf8142480236569ac80b36c64e3c0
jeremysr <jeremysr@protonmail.com>
2023-09-04 14:49:14 -0600

    removed a conosle.log

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,6 @@ const noteRouting = require('express').Router();
 const path = require('path');
 
 noteRouting.get("*", (req, res) => {
-    console.log(`GET request received at /notes`);
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 })
 
